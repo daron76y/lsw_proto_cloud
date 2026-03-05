a3f851ae865472d30b446547c8021152c3487a7f
Farid Zaki <147770092+faridmzaki@users.noreply.github.com>
2026-03-05 15:47:54 -0500

    implemented PvE AI Logic

--- battle/src/main/java/com/lsw_proto_cloud/battle/api/AIInputService.java
+++ battle/src/main/java/com/lsw_proto_cloud/battle/api/AIInputService.java
@@ -6,8 +6,44 @@ import com.lsw_proto_cloud.core.Party;
 import com.lsw_proto_cloud.core.Unit;
 
 public class AIInputService implements InputService {
+     private static final Random random = new Random();
+
     @Override
     public BattleCommand chooseBattleCommand(Unit unit, Party allyParty, Party enemyParty) {
-        return null;
+
+        List<Unit> enemies = enemyParty.getAliveUnits();
+        if (enemies.isEmpty()) {
+            return new WaitCommand(unit);
+        }
+        int choice = random.nextInt(4);
+
+        switch (choice) {
+            case 0: 
+                Unit attackTarget = enemies.get(random.nextInt(enemies.size()));
+                return new AttackCommand(unit, attackTarget);
+
+            case 1: 
+                return new DefendCommand(unit);
+
+            case 2: 
+                return new WaitCommand(unit);
+
+            case 3:
+                List<Ability> abilities = unit.getAbilities();
+                if (!abilities.isEmpty()) {
+                    Ability ability = abilities.get(random.nextInt(abilities.size()));
+                    Unit castTarget = null;
+                    if (ability.requiresTarget()) {
+                        castTarget = enemies.get(random.nextInt(enemies.size()));
+                    }
+                    return new CastCommand(unit, castTarget, allyParty, enemyParty, ability);
+                } else {
+                    
+                    Unit fallbackTarget = enemies.get(random.nextInt(enemies.size()));
+                    return new AttackCommand(unit, fallbackTarget);
+                }
+            default:
+                return new WaitCommand(unit);
+        }
     }
 }
